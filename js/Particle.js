db8b9e7aae748713113180d5222d04e99433a753
eyad <eyadsd6@gmail.com>
2019-03-22 03:41:02 +0200

    basic spring physics tested on 3 particles

--- js/Particle.js
+++ js/Particle.js
@@ -1,22 +1,46 @@
 class Particle{
     constructor(scene)
     {
-        var velocity = new THREE.Vector3();
-        var acceleration = new THREE.Vector3();
-        var mass = 1;
-        var force = new THREE.Vector3();
+        //this.model = new THREE.Mesh();
+        //this.velocity = new THREE.Vector3();
+        this.acceleration = new THREE.Vector3();
+        this.mass = 1;
+        this.force = new THREE.Vector3();
         this.scene = scene;
+        this.velocity = new THREE.Vector3();
     }
-    update()
+    update(delta)
     {
+        this.velocity.add(this.acceleration.clone().multiplyScalar(delta))
+        this.model.position.copy(this.model.position.clone().add(this.velocity.clone().multiplyScalar(delta)));
+        this.acceleration.multiplyScalar(0);
         
     }
     display()
     {
         var geometry = new THREE.BoxGeometry( 1, 1, 1 );
         var material = new THREE.MeshLambertMaterial( { color: 0x34495E  } );
-        cube1 = new THREE.Mesh( geometry, material );
-        cube1.position.set(0,2,0);
-        scene.add(cube1);
+        this.model = new THREE.Mesh( geometry, material );
+        this.model.position.set(0,2,0);
+        scene.add(this.model);
     }
+
+    addForce(newForce)
+    {
+        this.acceleration.add(newForce.multiplyScalar(1/this.mass));
+        
+    }
+    get position(){
+        return this.model.position;
+    }
+    set position(newPosition){
+        this.model.position.copy(newPosition);
+    }
+    get velocity(){
+        return this._velocity;
+    }
+    set velocity(newVelocity){
+        this._velocity = newVelocity;
+    }
+  
 }
--- js/Simulation.js
+++ js/Simulation.js
@@ -1,15 +1,12 @@
-var scene,camera,renderer,cube1,cube2,Fdrag,Fspring,velocity,position,mass,xRest,K,Fcube1,Fcube2;
+var scene,camera,renderer,cube1,cube2,Fdrag,Fspring,velocity,position,mass,xRest,K,particle1,particle2;
 
 
 function init(){
     clock =  new THREE.Clock;
     clock.start();
-    xRest = 5;
-    K = 0.1;
-    mass = 1;
-    Fcube1 = new THREE.Vector3();
-    Fcube2 = new THREE.Vector3();
-    Fcube3 = new THREE.Vector3();
+    xRest = 6;
+    K = 1;
+  
 
 
     scene = new THREE.Scene();
@@ -23,58 +20,49 @@ function init(){
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
     
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshLambertMaterial( { color: 0x34495E  } );
-    cube1 = new THREE.Mesh( geometry, material );
-    cube1.position.set(-2,0,0);
-    
-    scene.add( cube1);
-
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshLambertMaterial( { color: 0x34495E  } );
-    cube2 = new THREE.Mesh( geometry, material );
-    cube2.position.set(2,0,0);
-    scene.add( cube2 );
-    
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshLambertMaterial( { color: 0x34495E  } );
-    cube3 = new THREE.Mesh( geometry, material );
-    cube3.position.set(0,0,2);
-    scene.add( cube3 );
-
+   
 
    
     var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
     scene.add( light )
 
-    var particle = new Particle(scene);
-    particle.display();
+    particle1 = new Particle(scene);
+    particle1.display();
+    particle1.position = new THREE.Vector3(2,0,0);
 
+    particle2 = new Particle(scene);
+    particle2.display();
+    particle2.position = new THREE.Vector3(-2,0,0);
+
+    particle3 = new Particle(scene);
+    particle3.display();
+    particle3.position = new THREE.Vector3(0,2,0);
 }
 
 function update()
 {
-    var velocity;
     delta = clock.getDelta();
-    Fcube2.add(Fspring(cube2,cube1));
-    Fcube2.add(Fspring(cube2,cube3));
-
-    velocity = Fcube2.clone().multiplyScalar((mass) * delta);
-    Fcube2.add(velocity.clone().normalize().multiplyScalar(-0.03))
-    cube2.position.copy(cube2.position.clone().add(velocity));
+    particle1.addForce(Fspring(particle1,particle2));
+    particle1.addForce(Fspring(particle1,particle3));
+    particle1.addForce(particle1.velocity.clone().normalize().multiplyScalar(-0.5));
 
 
-    Fcube1.add(Fspring(cube1,cube2));
-    Fcube1.add(Fspring(cube1,cube3));
-    velocity = Fcube1.clone().multiplyScalar((mass) * delta);
-    Fcube1.add(velocity.clone().normalize().multiplyScalar(-0.03))
-    cube1.position.copy(cube1.position.clone().add(velocity));
+    particle2.addForce(Fspring(particle2,particle1));
+    particle2.addForce(Fspring(particle2,particle3));
+    particle2.addForce(particle2.velocity.clone().normalize().multiplyScalar(-0.5));
 
-    Fcube3.add(Fspring(cube3,cube2));
-    Fcube3.add(Fspring(cube3,cube1));
-    velocity = Fcube3.clone().multiplyScalar((mass) * delta);
-    Fcube1.add(velocity.clone().normalize().multiplyScalar(-0.03))
-    cube3.position.copy(cube3.position.clone().add(velocity));
+
+    particle3.addForce(Fspring(particle3,particle1));
+    particle3.addForce(Fspring(particle3,particle2));
+    particle3.addForce(particle3.velocity.clone().normalize().multiplyScalar(-0.5));
+
+
+
+    particle1.update(delta);
+    particle2.update(delta);
+    particle3.update(delta);
+
+    
 }
 
 var animate = function () {
@@ -89,12 +77,10 @@ var animate = function () {
 init();
 animate();
 
-function Fspring(cube1 , cube2)
+function Fspring(particle1 , particle2)
 { 
 
-    let distanceVector = cube1.position.clone().sub(cube2.position);
-
-
+    let distanceVector = particle1.position.clone().sub(particle2.position);
     let xCurrent = distanceVector.length();
     let direction = distanceVector.normalize();
     let stretch = xCurrent - xRest;
@@ -103,9 +89,7 @@ function Fspring(cube1 , cube2)
 
 function onDocumentMouseDown()
 {
-    cube2.position.x+=1;
-    cube2.position.y+=1;
-
+   // particle1.position = particle1.position.add(new THREE.Vector3(0,0,1))
 }
 
 
