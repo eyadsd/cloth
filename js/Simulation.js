6603ea3bed3389a59abe920fa6d6bad93232bf17
eyad <eyadsd6@gmail.com>
2019-03-23 02:05:55 +0200

    test physics on Lines

--- js/Simulation.js
+++ js/Simulation.js
@@ -1,4 +1,4 @@
-var scene,camera,renderer,mass,particle1,particle2, particle;
+var scene,camera,renderer,mass,particle1,particle2, particle,mesh;
 
 
 function init(){
@@ -24,52 +24,84 @@ function init(){
 
   
 
-    particle = new Array(10);
+    particle = new Array(100);
     for (let index = 0; index < 10; index++) {
-        particle[index] = new Particle(scene);
-        particle[index].display();
-        particle[index].position = new THREE.Vector3(0,0,(index )-7);
-        
+        for (let j = 0; j < 10; j++)
+        {
+            particle[index + 10 * j] = new Particle(scene);
+            particle[index + 10 * j].display();
+            particle[index  + 10* j].position = new THREE.Vector3(j/4,0,(index /4 )-7);
+        }
+
+
     }
-    
+    var geometry = new THREE.Geometry();
+    var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+
+    for (let i = 0; i<100; i++)
+    {
+        geometry.vertices.push(particle[i].position)
+    }
 
 
 
-}
+    mesh = new THREE.Line(geometry,material)
+
+    scene.add(mesh)
+ }
+
+
 
 function update()
 {
     let delta = clock.getDelta();
 
-    for (let index = 0; index < 9; index++) {
-        particle[index].addForce(Fspring(particle[index],particle[index + 1],4,1));
-        particle[index].addForce(particle[index].velocity.clone().normalize().multiplyScalar(-0.1));
+    for (let index = 0; index < 99; index++) {
+        if(index % 10 == 0)
+        {
+            continue;
+        }
+        particle[index].addForce(Fspring(particle[index],particle[index + 1],0.25,10));
+        particle[index].addForce(particle[index].velocity.clone().normalize().multiplyScalar(-0.5));
         particle[index].addForce(gravity())
         console.log(particle[index])
 
-        
+
+    }
+    for (let index = 1; index < 99; index++) {
+        if(index % 10 == 0 )
+        {
+            continue;
+        }
+        particle[index].addForce(Fspring(particle[index],particle[index - 1],0.25,10));
+        particle[index].addForce(particle[index].velocity.clone().normalize().multiplyScalar(-0.5));
+        particle[index].addForce(gravity())
+        console.log(particle[index])
+
+
     }
 
-
-    for (let index = 0; index < 10; index++) {
+    for (let index = 0; index < 99; index++) {
 
         particle[index].update(delta);
-        
+        mesh.geometry.vertices[index] = particle[index].position;
+        mesh.geometry.verticesNeedUpdate = true;
+
     }
 
 
 
 
- 
 
-    
+
+
 }
 
 var animate = function () {
 	requestAnimationFrame( animate );
-    
-	
-    
+
+
+
     update();
 	renderer.render( scene, camera );
 };
@@ -78,7 +110,7 @@ init();
 animate();
 
 function Fspring(point , anchor,xRest,K)
-{ 
+{
 
     let distanceVector = point.position.clone().sub(anchor.position);
     let xCurrent = distanceVector.length();
@@ -90,7 +122,7 @@ function Fspring(point , anchor,xRest,K)
 function gravity()
 {
 
-    return new THREE.Vector3(0,-9.8,0)
+    return new THREE.Vector3(0,-2,0)
 }
 function onDocumentMouseDown()
 {
